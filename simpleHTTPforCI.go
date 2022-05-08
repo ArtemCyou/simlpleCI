7354432710f01e065bc88f111f2a1fb2a7b329f2
Артём Прилуцких <prilutskikh@gmail.com>
2022-05-08 20:55:07 +0300

    edit server.go

--- simpleHTTPforCI.go
+++ simpleHTTPforCI.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
@@ -9,10 +10,19 @@ func hello(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, "hello server\n")
 }
 
+func headers(w http.ResponseWriter, req *http.Request) {
+	for name, headers := range req.Header {
+		for _, h := range headers {
+			fmt.Fprintf(w, "name: %s, value: %s\n", name, h)
+		}
+	}
+}
 
 func main() {
 	http.HandleFunc("/hello", hello)
+	http.HandleFunc("/headers", headers)
 
+	log.Println("start listening")
 
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
